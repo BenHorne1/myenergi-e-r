8835c54fa953ab4110e4685e6ae01219d2b8a90f
Ben Horne <ben.horne4335@gmail.com>
2023-08-30 17:15:30 +0100

    Added SideBar and page navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+import SendJSON from "./pages/SendJSON";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <BrowserRouter>
+        <SideBar />
+
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/send" element={<SendJSON />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
