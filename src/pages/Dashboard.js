3822b3f899cf12f62ebc6fce915518ef8a6facac
Ben Horne <ben.horne4335@gmail.com>
2023-08-30 17:24:22 +0100

    added new device button

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,25 @@
-export default function Dashboard() {
-    return (
-        <div className="ml-16 min-h-screen min-w-full bg-zinc-700">
-          <div className="bg-zinc-800 mb-2 shadow-md ">
-            <h1 className="mx-2 text-white bg-zinc-800 font-bold shadow-md">
-              Dashboard
-            </h1>
-          </div>
+import { AiOutlinePlus } from "react-icons/ai";
 
-        </div>
-      );
+export default function Dashboard() {
+  console.log("dashboard is being rendered");
+
+  const newDevice = () => {
+    console.log("adding a new device");
+  };
+
+  return (
+    <div className="ml-16 min-h-screen min-w-full bg-zinc-700">
+      <div className="bg-zinc-800 mb-2 shadow-md ">
+        <h1 className="mx-2 text-white bg-zinc-800 font-bold shadow-md">
+          Dashboard
+        </h1>
+      </div>
+      <button onClick={newDevice} className="add-device group HiOutlineCog">
+        <AiOutlinePlus size="28" />
+        <span className="sidebar-tooltip group-hover:scale-100">
+          Add a new device
+        </span>
+      </button>
+    </div>
+  );
 }
